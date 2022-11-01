2778f6b64c65e99a5e98321a5bc23d473e806c37
kolawole <84532090+Kolawole33@users.noreply.github.com>
2022-11-02 00:11:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,37 +8,43 @@ Created on Tue Nov  1 10:55:13 2022
 import streamlit as st
 import pickle
 from streamlit_option_menu import option_menu
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+
 
 spam_mail= pickle.load(open("spam.sav","rb"))
 
-with st.sidebar:
-    selected= option_menu("Prediction System",[
-        "Spam Mail"],
-        icons=["activity"],
-        default_index=0)
 
-if (selected == 'Spam Mail'):
+with st.sidebar:
+    selected= option_menu("Mail Prediction System",
+                          ["Spam Mail Prediction System"],
+                          icons=["activity"],
+                          default_index=0)
     
+if (selected == 'Spam Mail Prediction System'):
     # page title
-    st.title('Spam Mail using ML')
+    st.title('Spam Mail Prediction System using ML')
+    
+    #Mail= st.text_input('Enter Mail Message')
+    Message= st.text_input('Enter Mail Message')
+    
+    #vectorizer= TfidfVectorizer(min_df=1,stop_words="english",lowercase=True)
+    
+    #Message= vectorizer.fit_transform(Mail)
     
     
-    # getting the input data from the user
-    col1= st.columns(1)
+    spam_mail_= ""
     
-    with col1:
-        Message = st.text_input('Enter Your Mail')
+    if st.button('Mail Result'):
+      Spam_Mail_Prediction= spam_mail.predict([[Message]])
+      
+      if (Spam_Mail_Prediction[0]==1):
+          spam_mail_="This Mail is Spam"
+          
+      else:
+          spam_mail_="This Mail is Not Spam"
+          
+    st.success(spam_mail_)
     
-mail_spam= ""
     
-if st.button('Spam Mail Result'):
-    spam_prediction= spam_mail.predict([[Message]])
-        
-    if (spam_prediction[0] ==1):
-        mail_spam="This Mail is Spam"
-    else:
-        mail_spam="This Mail is Not Spam"
-        
-st.sucess(mail_spam)
-            
-        
