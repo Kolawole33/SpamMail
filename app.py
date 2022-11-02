bc2fc4e12c10ac613f38034b8f7a392cc9b9571d
kolawole <84532090+Kolawole33@users.noreply.github.com>
2022-11-02 17:46:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,12 +23,13 @@ port_stem= PorterStemmer()
 
 
 def pred_spam(content):
-    content= content.lower()
-    content= content.split()
-    content=[port_stem.stem(word) for word in content if not word in stopwords.words("english")]
-    content=" ".join(content)
+    for i in content:
+        n_content= i.lower()
+        n_content= n_content.split()
+        n_content=[port_stem.stem(word) for word in n_content if not word in stopwords.words("english")]
+        n_content=" ".join(n_content)
     
-    stemmed_content= feature_extraction.transform(content).toarray()
+    stemmed_content= feature_extraction.transform(n_content).toarray()
 
     prediction= spam_mail.predict(stemmed_content)
     
@@ -55,7 +56,7 @@ def main():
     
     #creating a button for prediction
     if st.button("spam Result"):
-        spam= pred_spam([Message])
+        spam= pred_spam([[Message]])
         
     st.success(spam)
 
